297270e323f69f401e81ddce16788127555897ec
Huy Le <huyle333@bu.edu>
2013-12-07 15:02:46 -0500

    changes

--- OnTimePhoneApp/OnTimePhoneApp/GreenLine.xaml.cs
+++ OnTimePhoneApp/OnTimePhoneApp/GreenLine.xaml.cs
@@ -7,66 +7,14 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using System.Threading.Tasks;
-using Microsoft.Phone.Maps.Controls;
-using System.Device.Location;
-using Windows.Devices.Geolocation; 
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace OnTimePhoneApp
 {
-    public static class CoordinateConverter
+    public partial class Page1 : PhoneApplicationPage
     {
-        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
-        {
-            return new GeoCoordinate
-                (
-                geocoordinate.Latitude,
-                geocoordinate.Longitude,
-                geocoordinate.Altitude ?? Double.NaN,
-                geocoordinate.Accuracy,
-                geocoordinate.AltitudeAccuracy ?? Double.NaN,
-                geocoordinate.Speed ?? Double.NaN,
-                geocoordinate.Heading ?? Double.NaN
-                );
-        }
-    }
-
-    public partial class GreenLine : PhoneApplicationPage
-    {
-      
-        public GreenLine()
+        public Page1()
         {
             InitializeComponent();
         }
-
-        private async void ShowMyLocationOnTheMap()
-        {
-            Geolocator myGeolocator = new Geolocator();
-            Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
-            Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
-            GeoCoordinate myGeoCoordinate = CoordinateConverter.ConvertGeocoordinate(myGeocoordinate);
-            this.map.Center = myGeoCoordinate;
-            this.map.ZoomLevel = 13;
-            Ellipse myCircle = new Ellipse();
-            myCircle.Fill = new SolidColorBrush(Colors.Blue);
-            myCircle.Height = 20;
-            myCircle.Width = 20;
-            myCircle.Opacity = 50;
-            MapOverlay myLocationOverlay = new MapOverlay();
-            myLocationOverlay.Content = myCircle;
-            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
-            myLocationOverlay.GeoCoordinate = myGeoCoordinate;
-            MapLayer myLocationLayer = new MapLayer();
-            myLocationLayer.Add(myLocationOverlay);
-            map.Layers.Add(myLocationLayer);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ShowMyLocationOnTheMap();
-        }
-        
     }
 }
